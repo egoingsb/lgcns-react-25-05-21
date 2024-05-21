5449fbd541250693923e4be577396a1de65b06aa
egoinig <egoing@gmail.com>
2024-05-21 15:48:53 +0900

    삭제 기긍 구현

--- src/components/contextMenu.tsx
+++ src/components/contextMenu.tsx
@@ -1,14 +1,21 @@
 'use client';
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export function ContextMenu(){
+    const router = useRouter();
     const params = useParams();
     if(params.id === undefined){
         return null;
     }
     return <>
         <li><Link href="/update/id">Update</Link></li>
-        <li><button>Delete</button></li>
+        <li><button onClick={async ()=>{
+            await fetch(`http://localhost:9999/pages/${params.id}`,{
+                method:'DELETE'
+            });
+            router.push('/');
+            router.refresh();
+        }}>Delete</button></li>
     </>
 }
