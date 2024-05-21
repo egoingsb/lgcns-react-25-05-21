70c7a0b4b47cfdac92c20cb28d6a147287aafb5c
egoinig <egoing@gmail.com>
2024-05-21 11:15:03 +0900

    기본 골격

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+        </ol>
+        <h2>Welcome</h2>
+        Hello, WEB!
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/update/id">Update</a></li>
+          <li><button>Delete</button></li>
+        </ul>
+      </body>
     </html>
   );
 }
